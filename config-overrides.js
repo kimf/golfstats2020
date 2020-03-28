b082973c5d60f2b55aaabe8510d589db14f9e4eb
Kim Fransman <kim.fransman@gmail.com>
2020-03-28 22:54:11 +0100

    babelRc support + babel-plugin-blade

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,8 @@
-const { override } = require("customize-cra");
+const { override, useBabelRc } = require("customize-cra");
 const { addReactRefresh } = require("customize-cra-react-refresh");
 
 /* config-overrides.js */
-module.exports = override(addReactRefresh({ disableRefreshCheck: true }));
+module.exports = override(
+  useBabelRc(),
+  addReactRefresh({ disableRefreshCheck: true })
+);
